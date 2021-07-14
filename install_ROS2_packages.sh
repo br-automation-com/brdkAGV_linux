738d10135bb4eb3831463332595e1bb9a9d08eec
Jacob Buhl <36692577+buhlj@users.noreply.github.com>
2021-07-14 20:53:37 +0200

    Create install_ROS2_packages.sh
    Added install file for packages

---
+++ install_ROS2_packages.sh
@@ -0,0 +1,13 @@
+cd ros_ws/src/
+
+# will either pull if the folder exist or clone it
+ echo "SICK safety scanner base"
+ [ -d "./sick_safetyscanners_base" ] && [ ! -L "./sick_safetyscanners_base/" ] && cd sick_safetyscanners_base/ && git pull && cd .. || git clone https://github.com/SICKAG/sick_safetyscanners_base
+ echo "SICK safety scanner interfaces"
+ [ -d "./sick_safetyscanners2_interfaces" ] && [ ! -L "./sick_safetyscanners2_interfaces/" ] && cd sick_safetyscanners2_interfaces/ && git pull && cd .. || git clone https://github.com/SICKAG/sick_safetyscanners2_interfaces
+ 
+ 
+ 
+ source /opt/ros/galactic/setup.bash
+ colcon build --symlink-install
+ source ~/colcon_ws/install/setup.sh
